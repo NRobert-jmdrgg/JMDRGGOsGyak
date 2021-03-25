9feb670d8c67e868254c53c8d893e66880926c87
Róbert <rob.iskola1@gmail.com>
2021-03-25 09:47:33 +0100

    jegyzokonyv

--- jmdrgg_0323/jmdrgg_openclose.c
+++ jmdrgg_0323/jmdrgg_openclose.c
@@ -8,12 +8,16 @@ int main(int argc, char const *argv[]) {
     int fileDescriptor = open("jmdrgg.txt", O_RDWR);
     
     if (fileDescriptor == -1) {
-        printf("Hibas file name");
+        fprintf(stderr, "Hibas file name");
         return 1;
     } 
 
     char text[256];
     int numberOfBytes = read(fileDescriptor, text, sizeof(text));
+
+    if (numberOfBytes == 0 || numberOfBytes == -1) {
+        fprintf(stderr, "Hiba volt a beolvasassal");
+    }
 
     text[numberOfBytes] = '\0';
 
@@ -23,7 +27,14 @@ int main(int argc, char const *argv[]) {
 
     char szoveg[] = "szoveg";
     numberOfBytes = write(fileDescriptor, szoveg, sizeof(szoveg));
+
+    if (numberOfBytes == 0 || numberOfBytes == -1) {
+        fprintf(stderr, "Hiba volt a fileba irassal");
+    }
+
     printf("\nkiirt szoveg: %s\nByteok szama: %d", szoveg, numberOfBytes);
+
+    close(fileDescriptor);
 
     return 0;
 }
