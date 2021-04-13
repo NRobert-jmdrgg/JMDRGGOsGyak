8e6e0a9e1881d1734e363a87a22e3ae13ca7143b
NRobert-jmdrgg <rob@DESKTOP-QM6SU7K.localdomain>
2021-04-13 19:59:56 +0200

    feleves init

---
+++ feleves_feladat/jmdrgg_feleves.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/*
+    6. Irjon C nyelvu programokat, ami
+    letrehoz egy nevesitett csovezeteket (bejegyzes az fs-en)
+    megnyitja
+    olvasni probal belole
+    kozben egy masik program ir bele egy random szamot
+    az elso program kiirja a kapott szamot es visszakuldi ennek a duplajat
+    a masik program kiolvassa es kiirja a kepernyore
+    a processzek megszunnek es a masodik program eltunteti a nevesitett csovezeteket"
+
+    Hatarido : maj 11.
+*/
+
+int main(int argc, char const *argv[]) {
+    
+    return 0;
+}
