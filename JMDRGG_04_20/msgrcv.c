2f07e619dd9cc0be76f0b1d120117b1c0d420126
NRobert-jmdrgg <rob@DESKTOP-QM6SU7K.localdomain>
2021-04-21 19:59:19 +0200

    uzenet megkapo

--- JMDRGG_04_20/msgrcv.c
+++ JMDRGG_04_20/msgrcv.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <errno.h>
 
-int main(int argc, char const *argv[]) {
-    
-    return 0;
+#define KEY 123456L
+
+typedef struct msgbuf {
+    long mtype;      //message type
+    char mtext[256]; //uzenet hossza legyen 256 byte
+} messageBuffer;
+
+void main() {
+    key_t key = KEY;
+
+    messageBuffer recieveBuffer;
+    messageBuffer *messagePointer;
+    messagePointer = &recieveBuffer;
+
+
+    int messageFlag = 0666 | IPC_CREAT | MSG_NOERROR;
+
+    int messageID = msgget(key, messageFlag);
+    if (messageID == -1) {
+        perror("Nem sikerult letrehozni\n");
+        exit(-1);
+    }
+    printf("Letrejott uzenet %d\n", messageID);
+
+    struct msqid_ds descriptor, *buffer;
+
+    buffer = &descriptor;
+    int messageLen = 20;
+    int messageType = 0; //minden tipus lehet
+    int messageReturn = msgctl(messageID, IPC_STAT, buffer);
+    if (messageReturn == -1) {
+        perror("Nem sikerult megkapni az uzenetet\n");
+        exit(-1);
+    }
+
+    printf("A megkapott üzenetek szama: %ld", buffer->msg_qnum);
+
+    while (buffer->msg_qnum) {
+        messageReturn = msgrcv(messageID, messagePointer, messageLen, messageType, messageFlag);
+        if (messageReturn == -1) {
+            perror("Nem sikerult megkapni az uzenetet\n");
+            exit(-1);
+        }
+
+        printf("Megkapott uzenet : %s", messagePointer->mtext);
+    }   
 }
