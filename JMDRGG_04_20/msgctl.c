7e78c0cc59e2b11d4093bc90f481d91a38d56316
NRobert-jmdrgg <rob@DESKTOP-QM6SU7K.localdomain>
2021-04-21 20:03:27 +0200

    msgctl

--- JMDRGG_04_20/msgctl.c
+++ JMDRGG_04_20/msgctl.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <errno.h>
 
-int main(int argc, char const *argv[]) {
-    
-    return 0;
+#define KEY 123456L
+
+void main() {
+    key_t key = KEY;
+    int messageID = msgget(key, IPC_CREAT | 0666);
+    int messageReturn = msgctl(messageID, IPC_RMID, NULL);
+    if (messageID == -1) {
+        perror("Nem sikerult kitorolni \n");
+        exit(-1);
+    }
+
+    printf("Sikerult kitorolni !!\n");
+
 }
