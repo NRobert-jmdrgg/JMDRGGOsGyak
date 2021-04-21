b27dcd1e6161f24328a482c9f579500b2c403e1c
NRobert-jmdrgg <rob@DESKTOP-QM6SU7K.localdomain>
2021-04-21 19:15:16 +0200

    uzenet kuldo

--- JMDRGG_04_20/msgcreate.c
+++ JMDRGG_04_20/msgcreate.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <errno.h>
 
-int main(int argc, char const *argv[]) {
+#define KEY 123456L
+
+typedef struct msgbuf {
+    long mtype;      //message type
+    char mtext[256]; //uzenet hossza legyen 256 byte
+} messageBuffer;
+
+
+
+void main() {
+    key_t key = KEY;
+    int messageId = msgget(key, IPC_CREAT | 0666);
+    if (messageId == -1) {
+        perror("\n Nem jott letre message queue!!\n");
+        exit(-1);
+    }
+    printf("\nA letrehozott uzenetros idje : %d\n", messageId);
     
-    return 0;
-}
+    messageBuffer message;
+    messageBuffer *messagePointer = &message;
+
+    messagePointer->mtype = 1;    //a kérés típusa 1
+    strcpy(messagePointer->mtext, "Ez az elso uzenet");
+    int messageLen = strlen(messagePointer->mtext) + 1;
+
+    int messageReturn = msgsnd(messageId, messagePointer, messageLen, 0666 | IPC_CREAT);
+    if (messageReturn == -1) {
+        perror("\nNem sikerult kuldeni!!\n");
+        exit(-1);
+    }
+
+    printf("Elkuldott uzenet: %s hossza : %d\n", messagePointer->mtext, messageLen);
+
+    strcpy(messagePointer->mtext, "Ez a masodik uzenet");
+    messageLen = strlen(messagePointer->mtext) + 1;
+
+    messageReturn = msgsnd(messageId, messagePointer, messageLen, 0666 | IPC_CREAT);
+    if (messageReturn == -1) {
+        perror("\nNem sikerult kuldeni!!\n");
+        exit(-1);
+    }
+
+    printf("Elkuldott uzenet: %s hossza : %d\n", messagePointer->mtext, messageLen);
+
+    printf("Sikeres üzenetküldések !!\n");
+}   
