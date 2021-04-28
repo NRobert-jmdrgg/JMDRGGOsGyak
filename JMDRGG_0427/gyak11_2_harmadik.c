654f349681ee1d0c3f8deac348b334e2b0705729
NRobert-jmdrgg <rob@DESKTOP-QM6SU7K.localdomain>
2021-04-28 22:54:36 +0200

    2.a

---
+++ JMDRGG_0427/gyak11_2_harmadik.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define KEY 77777L
+
+void main() {    
+    int semID = semget(KEY, 0, 0);
+    
+    if (semID == -1) {
+        perror("Nem sikerult megnyitni\n");
+        exit(-1);
+    }
+
+    semctl(semID, 0, IPC_RMID);
+}
