aa1f185d650b9b002d99c91c60d3ccc0f0106583
NRobert-jmdrgg <rob@DESKTOP-QM6SU7K.localdomain>
2021-04-29 22:13:32 +0200

    getall

--- JMDRGG_0427/semval.c
+++ JMDRGG_0427/semval.c
@@ -6,17 +6,29 @@
 #include <stdlib.h>
 #define KEY 123456L
 
+union semun {
+    int val;                 /* Value for SETVAL */
+    struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+    unsigned short *array;   /* Array for GETALL, SETALL */
+    struct seminfo *__buf;   /* Buffer for IPC_INFO (Linux-specific) */
+};
+
 void main() {
     
     int semID = semget(KEY, 0, 0);
+    int n = 5;
     if (semID == -1) {
         perror("Nem sikerult szemaforokat lekerdezni\n");
         exit(-1);
     }
 
-    printf("Szemaforok tartalma: \n");
+    union semun arg;
 
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", semctl(semID, i, GETVAL));
-    }   
+    printf("Szemaforok tartalma: \n");
+    arg.array = (short *)calloc(n, sizeof(int));
+
+    semctl(semID, 0, GETALL, arg);
+
+    for (int i = 0; i < n; i++) 
+        printf("%d ", arg.array[i]);   
 }
