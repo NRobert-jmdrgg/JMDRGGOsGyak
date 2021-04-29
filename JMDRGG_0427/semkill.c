e288d35313f45473f61b0b13926f0120702d579f
NRobert-jmdrgg <rob@DESKTOP-QM6SU7K.localdomain>
2021-04-29 22:17:06 +0200

    apro valtoztatasok

--- JMDRGG_0427/semkill.c
+++ JMDRGG_0427/semkill.c
@@ -7,14 +7,14 @@
 #define KEY 123456L
 
 void main() {
+    int n = 5;
     int semID = semget(KEY, 0, 0);
     if (semID == -1) {
         perror("Nem sikerult szemaforokat lekerdezni\n");
         exit(-1);
     }
 
-
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < n; i++)
         semctl(semID, i, IPC_RMID);
 
 }
